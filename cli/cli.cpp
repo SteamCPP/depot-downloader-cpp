6abbc1d0556c7b4e63b1c872d876da4c0008dd63
lammmab <liamlam8282@gmail.com>
2026-05-06 19:52:50 -0400

    Add preset .vscode settings and CMakePresets

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -1,5 +1,6 @@
 #include <CLI11.hpp>
 
 int main(int argc, char** argv) {
-    CLI::App app{"App description"};
+    CLI::App depot_dl{"Depot downloader tool written in C++"};
+    argv = depot_dl.ensure_utf8(argv);
 }
